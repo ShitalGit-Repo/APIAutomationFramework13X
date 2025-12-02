d77eff9cf4e3994c247ae8fddf2f9a6b6d648abf
IML-WKS-Admin <awacharshital94@gmail.com>
2025-12-02 15:25:11 -0500

    2 December 2025

---
+++ src/main/java/com/endpoints/APIConstants.java
@@ -0,0 +1,17 @@
+package com.endpoints;
+
+public class APIConstants {
+
+//Project 1 - Restful Booker
+    public static String BASE_URL = "https://restful-booker.herokuapp.com/" ;
+    public static String CREATE_UPDATE_BOOKING_URL = "/booking" ;
+    public static String AUTH_URL = "/auth" ;
+    public static String PING_URL = "/ping" ;
+
+//Project 2 - VWO login
+    public static String APP_VWO_URL = "https://app.vwo.com/login" ;
+
+//Project 3
+    public static String BASE_URL_PROJECT_3 = "";
+    public static String UPDATE_STATUS  = "/api/v1/trip/update-status";
+}
