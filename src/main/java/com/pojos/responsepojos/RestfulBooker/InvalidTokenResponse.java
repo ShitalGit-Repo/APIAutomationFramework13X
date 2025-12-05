60b867c60c652bf02b38fd601615311064c25dda
IML-WKS-Admin <awacharshital94@gmail.com>
2025-12-05 13:51:19 -0500

    5 December 2025

---
+++ src/main/java/com/pojos/responsepojos/RestfulBooker/InvalidTokenResponse.java
@@ -0,0 +1,14 @@
+package com.pojos.responsepojos.RestfulBooker;
+
+public class InvalidTokenResponse {
+
+    private String reason;
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
