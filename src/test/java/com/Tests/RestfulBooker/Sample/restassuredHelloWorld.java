cb6254cd566004e9bf9283d761ce2e6b581ecac8
IML-WKS-Admin <awacharshital94@gmail.com>
2025-12-07 11:48:35 -0500

    7 Dec 2025

---
+++ src/test/java/com/Tests/RestfulBooker/Sample/restassuredHelloWorld.java
@@ -0,0 +1,8 @@
+package com.Tests.RestfulBooker.Sample;
+
+public class restassuredHelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hi I am working");
+    }
+}
